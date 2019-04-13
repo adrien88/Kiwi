3d1bb40780692f8b66adf0f22a446229c155adac
adrien <adrien@Nunux.lan>
2019-04-14 00:16:34 +0200

    moving units tests

--- index.php
+++ index.php
@@ -7,69 +7,6 @@ spl_autoload_register(function ($class) {
     include 'includes/' . $class . '.class.php';
 });
 
-echo '<br>Tests unitaires : <br><pre>';
-
-$BDD = new EPDO2();
-
-echo '<br><b>Structure :<b> ';
-print_r($BDD->getStruct('pages'));
-
-
-if ($BDD->insert([
-    'id_owner'=>1,
-    'url'=>'exemple',
-    'title'=>'exemple n°1',
-    'description'=>'exemple',
-    'content'=>'exemple',
-    'publication'=>time(),
-    'thumbnail'=>'',
-    'keywords'=>'',
-])){
-    echo '<br><b>Insert success.</b><br>';
-}
-
-if ($BDD->update(['title'=>'exemple n°4','url'=>'exemple2'],['url'=>'exemple'])){
-    echo '<br><b>Update success.</b><br>';
-}
-
-if ($BDD->delete(['url'=>'exemple'])){
-    echo '<br><b>Delete success.</b><br>';
-};
-
-echo '<br><b>Content :<b> ';
-print_r($BDD->query('SELECT url,title FROM pages'));
-
-
-
-
-echo '</pre><br>/pre';
-
-// associate
-// EPDO::getInstance()->getTable('pages');
-//
-// print_r(EPDO::do()->insert([
-//     'id_owner'=>1,
-//     'url'=>'exemple',
-//     'title'=>'exemple n°1',
-//     'description'=>'exemple',
-//     'content'=>'exemple',
-//     'publication'=>time(),
-//     'thumbnail'=>'',
-//     'keywords'=>'',
-// ]));
-//
-// echo '<br>';
-// print_r(EPDO::do()->delete(['url'=>'exemple2']));
-// echo '<br>';
-// print_r(EPDO::do()->query('SELECT title FROM pages WHERE url=\'exemple\''));
-// echo '<br>';
-// print_r(EPDO::do()->update(['url'=>'exemple2','title'=>'exemple n°2'],['url'=>'exemple']));
-// echo '<br>';
-// print_r(EPDO::do()->query('SELECT title FROM pages WHERE url=\'exemple2\''));
-// echo '<br><pre>';
-// print_r(EPDO::do()->getStruct());
-// echo '</pre>';
-
 
 // Config::save('test.ini',
 //     [
@@ -87,30 +24,4 @@ echo '</pre><br>/pre';
 //     ]);
 //
 // echo '<br><br><pre>'.print_r(Config::load('test.ini'),1).'</pre>';
-//
-
-
-// class boolean {
-//     final public static function true(){
-//         if(true){
-//             return true;
-//         }
-//     }
-//     final public static function false(){
-//         if(false){
-//             return false;
-//         }
-//     }
-// }
-//
-//
-// class kamikaze {
-//     public function __construct(){
-//         $this->__destruct();
-//     }
-//     public function __destruct(){
-//         return 0;
-//     }
-// }
-//
 //
---
+++ unit_test/EPDO.php
@@ -0,0 +1,43 @@
+<?php
+
+echo 'Ok';
+
+include '../includes/EPDO2.class.php';
+
+if (class_exists('EPDO2')){
+    echo 'Ok';
+}
+
+//
+// echo '<br>Tests unitaires : <br><pre>';
+//
+// $BDD = new EPDO2();
+//
+// echo '<br><b>Structure :<b> ';
+// print_r($BDD->getStruct('pages'));
+
+// if ($BDD->insert([
+//     'id_owner'=>1,
+//     'url'=>'exemple',
+//     'title'=>'exemple n°1',
+//     'description'=>'exemple',
+//     'content'=>'exemple',
+//     'publication'=>time(),
+//     'thumbnail'=>'',
+//     'keywords'=>'',
+// ])){
+//     echo '<br><b>Insert success.</b><br>';
+// }
+//
+// if ($BDD->update(['title'=>'exemple n°4','url'=>'exemple2'],['url'=>'exemple'])){
+//     echo '<br><b>Update success.</b><br>';
+// }
+//
+// if ($BDD->delete(['url'=>'exemple'])){
+//     echo '<br><b>Delete success.</b><br>';
+// }
+//
+// echo '<br><b>Content :<b> ';
+// print_r($BDD->query('SELECT url,title FROM pages'));
+//
+// echo '</pre><br>/pre';
