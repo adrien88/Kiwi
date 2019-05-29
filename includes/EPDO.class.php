1475783a19da574e7f6be843f83e7b69a31d6774
Adrien Boilley <adrien.boilley@gmail.com>
2019-05-29 09:40:10 +0200

    I prepare a new EPDO librairie.

---
+++ includes/EPDO.class.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+class EPDO {
+
+    #
+    public function __construct(){
+
+    }
+
+    public function query() {
+        try {
+            ## get db && get table(+regex)
+            ## create hendler
+            ## apply query
+            ## return result
+        }
+        catch (ERROR $e){
+            ## error handling
+        }
+    }
+
+    public function add() {
+        try {
+            ## get db && get table(+regex)
+            ## create hendler
+            ## apply query
+            ## return result
+        }
+        catch (ERROR $e){
+            ## error handling
+        }
+    }
+
+
+    public function edit() {
+        try {
+            ## get db && get table(+regex)
+            ## create hendler
+            ## apply query
+            ## return result
+        }
+        catch (ERROR $e){
+            ## error handling
+        }
+    }
+
+
+    public function delete() {
+        try {
+            ## get db && get table(+regex)
+            ## create hendler
+            ## apply query
+            ## return result
+        }
+        catch (ERROR $e){
+            ## error handling
+        }
+    }
+
+
+}
