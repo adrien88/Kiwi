5958e7b02280d5ea88e3ccb50f3373c5a708cf2c
Adrien Boilley <adrien.boilley@gmail.com>
2019-06-10 00:22:59 +0200

    Working on epdo traits.

---
+++ includes/DBHandler.trait.php
@@ -0,0 +1,125 @@
+<?php
+
+trait DBHandler {
+
+    // PDO instances
+    private static $PDO = [];
+    /**
+    * PDO [
+    *   dbname => [
+    *       dbo => PDO object
+    *       tableList => []
+    *       ]
+    *   ]
+    */
+
+
+
+    /** __________________________________________________________________________________
+    *   Create instance in static PDO
+    *   @param array:['name','host','login','passwd','charset']
+    *   @return void
+    *
+    */
+    final public static function connectDB(array $DB)
+    {
+        if (isset($DB['name']) && !isset(self::$PDO[$DB['name']])) {
+
+            // Init DB connect
+            try {
+                $req = $DB['type'].':dbname='.$DB['name'].';host='.$DB['host'].';';
+                $options = [
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ".$DB['charset']
+                ];
+
+                // instnce PDO in var
+                self::$PDO[$DB['name']]['dbo'] = new PDO ($req,$DB['login'],$DB['passwd'],$options);
+                self::$PDO[$DB['name']]['tables'] = self::tableList($DB['name'],'TABLE_NAME');
+
+                // print_r(self::$PDO[$DB['name']]['tables']);
+
+                // unset connecting params
+                unset($DB);
+            }
+            catch (PDOException $e){
+                // print error
+                print('Database connection error : '.$e);
+                // unset PDO
+                unset(self::$PDO[$DB['name']]);
+            }
+        }
+    }
+
+
+    /** __________________________________________________________________________________
+    *   select a database object (if selectable or selected)
+    *   @param database:string
+    *   @return success:database:object
+    *   @return error:false
+    */
+    final public static function issetInstance(string $database = null)
+    {
+        if(isset($database) && isset(self::$PDO[$database]['dbo'])){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /** __________________________________________________________________________________
+    *   select a database object (if selectable or selected)
+    *   @param database:string
+    *   @return success:database:object
+    *   @return error:false
+    */
+    final public static function getInstance(string $database = null)
+    {
+        if(isset($database) && isset(self::$PDO[$database]['dbo'])){
+            return self::$PDO[$database]['dbo'];
+        }
+        else {
+            return false;
+        }
+    }
+
+    /** __________________________________________________________________________________
+    *   unconnectDB
+    *   @param database:string
+    *   @return bool
+    */
+    final public static function unconnectDB($database = null) : bool
+    {
+        if (isset(self::$PDO[$database])) {
+            self::$PDO[$database] = null;
+            return true;
+        }
+        return false;
+    }
+
+    /** __________________________________________________________________________________
+    *   get table list
+    *   @param string tablename
+    *   @return array (list of tables names)
+    */
+    final public static function tableList(string $dbname, string $elem = '*') : array
+    {
+        $req = "SELECT $elem FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='$dbname';";
+
+        if (isset(self::$PDO[$dbname])) {
+
+            $list = self::$PDO[$dbname]['dbo']->query($req);
+            $tab = [];
+
+            // warning : fetchAll musn't be a boolean !
+            if (!is_bool($list)){
+                foreach($list->fetchAll() as $col){
+                    $tab[] = $col;
+                }
+            }
+        }
+        return $tab;
+    }
+}
---
+++ includes/QueryHandler.class.php
@@ -0,0 +1,174 @@
+<?php
+
+/** __________________________________________________________________________________
+*   DATA manipulation methods
+*
+*   query ( request(string), [Fetch_Method_Const] ) : mixed
+*
+*   insert ( data(array), [tablename(string)] ) : bool
+*
+*   update ( data(array), condition(array), [tablename(string)] ) : bool
+*
+*   delete ( condition(array), [tablename(string)] ) : bool
+*
+*/
+class EpdoQueryHandler {
+
+    private $PDO;
+    private $tablename;
+
+
+    public function __construct(PDO $PDO, string $tablename) {
+        $this->PDO = $PDO;
+        $this->tablename = $tablename;
+    }
+
+
+    /** __________________________________________________________________________________
+    *   send query request into table
+    *   @param : string query
+    *   @return success:array
+    *   @return error:false:throw:error_message
+    */
+    final public function query($req, $FETCH = null)
+    {
+        $stat = $this->PDO->prepare($req);
+        $stat->execute();
+        if(
+            ($error = $stat->errorInfo()[2])
+            && !empty($error)
+        ){
+            return $error;
+        }
+        else {
+            if (!is_bool($stat)){
+                $data = $stat->fetchAll($FETCH);
+                unset($stat);
+                if(!empty($data)){
+                    if(count($data) > 1 ){
+                        return $data;
+                    }
+                    else {
+                        return $data[0];
+                    }
+                }
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    /** __________________________________________________________________________________
+    *   query a psecific patter data into table
+    *   @param : string query
+    *   @return success:array
+    *   @return error:false:throw:error_message
+    *   
+    */
+
+    // final public function search(array $pattern, $cols = '*')
+    // {
+    //     $str = '';
+    //     foreach($pattern as $colname => $patt){
+    //         $str .= $colname.' LIKE '.$patt.' AND';
+    //     }
+    //     $req = 'SELECT '.$cols.' WHERE '.$str.';';
+    //     return $this->query($req);
+    // }
+
+    /** __________________________________________________________________________________
+    *   Insert data into table
+    *   @param : string tablename; array : data []
+    *   @return success:true
+    *   @return error:(string)errormessage
+    *
+    */
+    final public function insert(array $data,$table = null)
+    {
+        // formater la requête
+        $prepreq = implode(',',array_keys($data));
+        $prepval = ':'.implode(',:',array_keys($data));
+
+        // Sand
+        $req = "INSERT INTO ".$this->tablename." ($prepreq) VALUES ($prepval);";
+        $stat = $this->PDO->prepare($req);
+        $stat->execute($data);
+        if(
+            ($error = $stat->errorInfo()[2]) &&
+            !empty($error)
+        ) {
+            return $error."\nreq: ".$req."\n\n";
+        }
+        else {
+            return true;
+        }
+    }
+
+    /** __________________________________________________________________________________
+    *   Update data into table
+    *   @param : string tablename; array : data []
+    *   @return success:true
+    *   @return error:(string)errormessage
+    *
+    */
+    final public function update(array $data = [], array $cond = [],$table = null)
+    {
+
+        $sdata = [];
+        // write request
+        $req = 'UPDATE '.$this->tablename.' SET ';
+        foreach ($data as $key => $value) {
+            $req .= ''.$key.' = :'.$key.', ';
+        }
+        $req = substr($req,0,-2);
+
+        $req.=' WHERE ';
+        foreach ($cond as $key => $value) {
+            $req .= ''.$key.' = '.self::$PDO->quote($value).' AND ';
+        }
+        $req = substr($req,0,-4).';';
+
+        // Sand
+        $stat = $this->PDO->prepare($req);
+        $stat->execute($data);
+        if(
+            ($error = $stat->errorInfo()[2]) &&
+            !empty($error)
+        ){
+            return $error."\nreq: ".$req."\n\n";
+        }
+        else {
+            return true;
+        }
+    }
+
+
+    /** __________________________________________________________________________________
+    *   Delete data from table
+    *   @param : string tablename; array : data []
+    *   @return success:bool
+    *   @return error:(string)errormessage
+    *
+    */
+    final public function delete(array $cond,$table = null)
+    {
+        $req = 'DELETE FROM '.$this->tablename.' WHERE ';
+        foreach ($cond as $key => $value) {
+            $req .= ''.$key.' = :'.$key.' AND ';
+        }
+        $req = substr($req,0,-4).';';
+        $stat = $this->PDO->prepare($req);
+        $stat->execute($cond);
+        if(
+            ($error = $stat->errorInfo()[2])
+            && !empty($error)
+        ){
+            return $error."\nreq: ".$req."\n\n";
+        }
+        else {
+            return true;
+        }
+    }
+
+}
